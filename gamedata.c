b222968e6df2fe8cb2688434cb5e5b09839ff55e
HalfButterChickenGravy <pratikmayekar4@gmail.com>
2016-11-01 00:10:33 +0530

    Add files via upload

---
+++ gamedata.c
@@ -0,0 +1,8 @@
+#include "gamedata.h"
+#include <limits.h>
+
+
+void initgame(player *p) {
+	p->score = 0;	
+	p->i = INT_MAX;
+}
---
+++ gamedata.h
@@ -0,0 +1,22 @@
+#ifndef __GAMEDATA_H
+#define __GAMEDATA_H
+
+
+#include "obstacles.h"
+
+typedef struct player {
+	int score;
+	int i; //reverseframeno
+	char ch; //userinput
+	int obsloc;
+	int dinopos;
+	obstacle *o;
+//	p_character type;	
+	
+}player;
+
+
+
+void initgame(player *p);
+
+#endif
