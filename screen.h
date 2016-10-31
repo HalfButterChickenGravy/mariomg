ed6fd4cb6be384c12098c182d773d63ee3f81df6
HalfButterChickenGravy <pratikmayekar4@gmail.com>
2016-11-01 01:31:59 +0530

    Add files via upload

---
+++ screen.h
@@ -0,0 +1,11 @@
+#ifndef __GAMEDATA_H
+#define __GAMEDATA_H
+
+#include "dino.h"
+#include "gamedata.h"
+
+void print_dino(int pos, int frameno);
+void disp (player *p);
+void dino (player *p);
+
+#endif
