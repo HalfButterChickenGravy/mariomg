857df6b964f78ee82d146094e4388612cc43dc94
HalfButterChickenGravy <pratikmayekar4@gmail.com>
2016-11-01 00:11:08 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,27 +1,61 @@
 #include <ncurses.h>
 #include <stdio.h>
 #include <limits.h>
-#include "screen.h"
+#include <pthread.h>
+#include "gamedata.h"
+
+
+void *dino_runner (void *arg) {
+	player *p = (player*) arg;
+
+	while((p->ch = getch()) != 'q') {
+		p->score++;
+		clear();	
+		disp(p);
+		dino(p);
+		p->i--;	
+		timeout(70);
+	}
+	
+	pthread_exit(0);
+}
+
+void *obs_runner (void *arg) {
+	player *p = (player*) arg;
+	if(p->score % 150 == 0) {
+		addobstacles(p);
+
+	}
+	
+	pthread_exit(0);
+}
 
 
 int main() {
 	
 
-	int i;
-	char ch;
-	i = INT_MAX;
+	
 	initscr();
 	nodelay(stdscr, true);
 	keypad(stdscr, true);
 	noecho();
 	curs_set(0);
 	clear();
-	while((ch = getch()) != 'q') {
-		clear();
-		disp(i);
-		dino(i--, ch);
-		timeout(70);
-			
-	}
+	player *p;
+	initgame(p);
+	initobs(p->o);
+
+	pthread_t t_dino, t_obs;
+	pthread_attr_t attr;
+	pthread_attr_init(&attr);
+	
+	
+	//dinorunner
+	pthread_create(&t_dino, &attr, dino_runner, p);
+
+	//obsrunner
+	pthread_create(&t_obs, &attr, obs_runner, p);
+	
+	pthread_join(t_obs, NULL);
 	endwin();
 }
