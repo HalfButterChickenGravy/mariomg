a7ac3da8fa88ad80cf3f346abe60b3e9734ce432
HalfButterChickenGravy <pratikmayekar4@gmail.com>
2016-11-02 21:47:58 +0530

    Update obstacles.h

--- obstacles.h
+++ obstacles.h
@@ -1,3 +1,16 @@
+/* mariomg -- A simple implementation of offline dino runner game of Google Chrome in c
+   Copyright pratikmayekar4@gmail.com Pratik Mayekar
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
 #ifndef __OBSTACLES_H
 #define __OBSTACLES_H
 
@@ -12,10 +25,10 @@ typedef struct obstacle {
 	int height;
 } obstacle;
 
-void initobs(obstacle *tree);
+void initobs(player *p);
 void printobstacle (obstacle *x, int obsloc);
 int checkcollision (obstacle *x, int obsloc, int dinopos);
 int addobstacles (player *p);
-
+void assignobs (player *p);
 
 #endif
