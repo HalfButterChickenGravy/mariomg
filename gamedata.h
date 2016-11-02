6aeb386069a9797cbb259e3dd2dd6f4ae8b42c32
HalfButterChickenGravy <pratikmayekar4@gmail.com>
2016-11-02 21:46:08 +0530

    Update gamedata.h

--- gamedata.h
+++ gamedata.h
@@ -1,3 +1,17 @@
+/* mariomg -- A simple implementation of offline dino runner game of Google Chrome in c
+   Copyright pratikmayekar4@gmail.com Pratik Mayekar
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+
 #ifndef __GAMEDATA_H
 #define __GAMEDATA_H
 
@@ -6,12 +20,15 @@ typedef struct obstacle obstacle;
 
 typedef struct player {
 	int score;
+	int best;
 	int i; //reverseframeno
 	char ch; //userinput
 	int obsloc;
+	int obstype;
 	int dinopos;
 	obstacle *o;
-//	p_character type;	
+	int state;
+	int speed;
 	
 } player;
 #include "screen.h"
